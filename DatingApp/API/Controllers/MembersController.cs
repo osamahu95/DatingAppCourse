94687b016d038ab3c5f5aa8e502c49d1d8b5ce42
osamahu95 <osamahu95@gmail.com>
2026-01-18 22:14:01 +0500

    udemy/#dotnet_with_angular/section2_Members_controller_with_basic_get_functions_list_and_by_Id

---
+++ DatingApp/API/Controllers/MembersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext context = context;
+        
+        [HttpGet]
+        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        {
+            var members = context.Users.ToList();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetMember(string id)
+        {
+            var member = context.Users.Find(id);
+
+            if (member == null) return NotFound();
+
+            return member;
+        }
+    }
+}
