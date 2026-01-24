85976ae995a3c97731f10afc085768edf4c65eae
osamahu95 <osamahu95@gmail.com>
2026-01-24 22:58:44 +0500

    udemy/#dotnet_with_angular/section3_displayed_signal_value_in_main_index_page_and_added_http_client_in_app_config_or_module_and_utlized_in_out_app_to_call_our_apis

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
-  protected readonly simpleTitle = 'simple title in app';
+export class App implements OnInit{
+  private http = inject(HttpClient); // Use Inject as per latest angular guide
+  protected readonly title = signal('Dating App');
+  
+  // constructor(private http: HttpClient){} // old angular way - feels nostalgia. 
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log("Completed the Http Request")
+    })
+  }
 }
