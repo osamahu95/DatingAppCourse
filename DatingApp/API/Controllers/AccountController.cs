19c4f5530d7d46018cd2a04fc8623ec1df985aff
osamahu95 <osamahu95@gmail.com>
2026-01-25 22:26:43 +0500

    udemy/#dotnet_with_angular/section4_added_login_endpoint_in_accounts_controller_with_logindto

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -33,6 +33,24 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")] // api/account/login
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Email == loginDto.Email);
+            if (user == null) return Unauthorized("Invalid Email Address");
+
+            using var hmac = new HMACSHA512(user.PaswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
+            return user;
+        }
+
         #region HelperFunctions
         private async Task<bool> EmailExists(string email)
         {
