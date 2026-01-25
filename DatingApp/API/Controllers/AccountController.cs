f45f44b4bcfbf630b68ea469c8e9558a874e4ff6
osamahu95 <osamahu95@gmail.com>
2026-01-25 21:19:46 +0500

    udemy/#dotnet_with_angular/section4_new_account_controller_with_initial_register_signup_method

---
+++ DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(AppDbContext context) : BaseApiController
+    {
+        private readonly AppDbContext context = context;
+
+        [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+        {
+            var hmac = new HMACSHA512();
+
+            var user = new AppUser 
+            {
+                DisplayName = displayName,
+                Email = email,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PaswordSalt = hmac.Key,
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
