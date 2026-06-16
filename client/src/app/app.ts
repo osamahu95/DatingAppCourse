9da5547f90e419b3c4bb2aacd957f1589b5eaef5
osamahu95 <osamahu95@gmail.com>
2026-06-16 11:09:54 +0500

    udemy/#dotnet_with_angular/section6_used_router_outlet_to_switch_between_different_components_based_on_url_and_added_new_components_in_features

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,17 +3,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit{
   private http = inject(HttpClient); // Use Inject as per latest angular guide
+  protected router = inject(Router);
   protected readonly title = signal('Dating App');
   protected members = signal<User[]>([]);
   private accountService = inject(AccountService);
