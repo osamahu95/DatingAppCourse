1457372e48f337c885f81e6eee6b345881e56eff
osamahu95 <osamahu95@gmail.com>
2026-07-01 18:32:38 +0500

    udemy/#dotnet_with_angular/section7_handled_400_authentication_error_set_from_intereceptor_to_error_component

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -11,6 +11,18 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       if (error) {
         switch(error.status) {
           case 400:
+            if (error.error.errors) {
+              const modelStateErrors = [];
+              for (const key in error.error.errors) {
+                if (error.error.errors[key]) {
+                  modelStateErrors.push(error.error.errors[key]);
+                }
+              }
+
+              throw modelStateErrors.flat();
+              break;
+            }
+
             toast.error(error.error);
             break;
           case 401:
