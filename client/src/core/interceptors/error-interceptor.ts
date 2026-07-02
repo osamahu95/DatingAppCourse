265a1dd708cd2ce5d38bf66f5c43c16893240b57
osamahu95 <osamahu95@gmail.com>
2026-07-02 12:47:01 +0500

    udemy/#dotnet_with_angular/section7_created_path_for_server_error_component_and_passed_error_active_nagiation_router_server_error_componet_from_interceptor_and_redirected_to_server_component_route_and_utlized_the_state_passed_in_server_error_component_to_use_it_in_the_server_error_component_to_display_error_with_details_moreover_created_type_for_api_error_details

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError } from 'rxjs';
 import { ToastService } from '../services/toast-service';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toast = inject(ToastService);
@@ -34,7 +34,8 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
             router.navigateByUrl('/not-found');
             break;
           case 500:
-            toast.error('Server Error');
+            const navigationExtras: NavigationExtras = { state: { error: error.error } };
+            router.navigate(['/server-error'], navigationExtras);
             break;
           default:
             toast.error('Something went Wrong');
