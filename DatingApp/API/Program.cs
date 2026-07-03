2ba799aa317770ec7b9da7eb7a4c8ac3b954c342
osamahu95 <osamahu95@gmail.com>
2026-07-03 15:39:03 +0500

    udemy/#dotnet_with_angular/section8_seeded_the_data_inside_database

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -46,4 +46,19 @@ app.UseAuthorization();
 // Configure the HTTP request pipeline.
 app.MapControllers();
 
+// Create the database if not created, and migrate the data from json file if not exists.
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An Error Occured during migration");
+}
+
 app.Run();
