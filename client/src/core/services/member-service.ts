83fc33cc7ebb85b8acf3b90c6e0562120d42c571
osamahu95 <osamahu95@gmail.com>
2026-07-06 16:12:40 +0500

    udemy/#dotnet_with_angular/section9_created_jwt_interceptor_to_add_auth_bearer_token_inside_the_request_header_and_configured_it_in_the_provideHttpClient_in_app_config_ts_file

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Member } from '../../types/member';
@@ -13,18 +13,10 @@ export class MemberService {
   private baseUrl = environment.apiUrl;
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions())
+    return this.http.get<Member[]>(this.baseUrl + 'members')
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + `members/${id}`, this.getHttpOptions());
-  }
-
-  private getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + `members/${id}`);
   }
 }
