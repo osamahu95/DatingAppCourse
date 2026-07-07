147d693361c52d091c31bc0722abfd4e5f3f74ab
osamahu95 <osamahu95@gmail.com>
2026-07-07 16:47:01 +0500

    udemy/#dotnet_with_angular/section9_worked_on_theme_implementation_user_can_select_multiple_daisy_ui_themes

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToastService } from '../../core/services/toast-service';
 import { LoginCreds } from '../../types/user';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import { LoginCreds } from '../../types/user';
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
+export class Nav implements OnInit{
   protected accountsService = inject(AccountService);
   protected router = inject(Router);
   private toast = inject(ToastService);
@@ -19,6 +20,21 @@ export class Nav {
     email: '',
     password: ''
   };
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+
+    document.documentElement.setAttribute('data-theme', theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) elem.blur();
+  }
 
   login(){
     this.accountsService.login(this.creds).subscribe({
