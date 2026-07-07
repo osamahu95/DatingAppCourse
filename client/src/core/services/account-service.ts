023bd4c48dbbdeed5580462929001e6c917510b6
osamahu95 <osamahu95@gmail.com>
2026-07-07 19:26:51 +0500

    udemy/#dotnet_with_angular/section9_removed_direct_utlilization_of_env.dev.ts

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
