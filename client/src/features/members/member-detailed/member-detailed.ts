a11f53b356ccd68a3d6bbeb59659ace7c7e9f24c
osamahu95 <osamahu95@gmail.com>
2026-07-07 16:27:36 +0500

    udemy/#dotnet_with_angular/section9_created_custom_pipe_for_transform_dob_to_age_to_display_also_a_small_fix_for_nav_when_access_matches_it_highlights_but_when_selected_any_member_it_was_highlighted_still_so_I_applied_routeLinkActive_property_exact_true_fixed_this_issue

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -2,13 +2,15 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Member } from '../../../types/member';
+import { AgePipe } from '../../../core/pipes/age-pipe';
 
 @Component({
   selector: 'app-member-detailed',
   imports: [
     RouterLink, 
     RouterLinkActive, 
-    RouterOutlet
+    RouterOutlet,
+    AgePipe
   ],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
