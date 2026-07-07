25b6331a987f3e7a2b2d38268014ac6cb2050b36
osamahu95 <osamahu95@gmail.com>
2026-07-07 13:01:08 +0500

    udemy/#dotnet_with_angular/section9_added_rules_for_resolvers_in_schematics_to_be_added_in_certain_path_made_a_new_resolver_which_is_fetch_data_from_memberbyId_api_before_the_navigation_is_resolved_and_then_we_used_that_data_in_our_components

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,14 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
-import { filter, Observable } from 'rxjs';
+import { filter } from 'rxjs';
 import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detailed',
   imports: [
-    AsyncPipe, 
     RouterLink, 
     RouterLinkActive, 
     RouterOutlet
@@ -17,16 +14,17 @@ import { Member } from '../../../types/member';
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit{
-  private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
   
   ngOnInit(): void {
-    this.member$ = this.loadMember();
-    this.title.set(this.route.firstChild?.snapshot?.title);
+    this.route.data.subscribe({
+      next: data => this.member.set(data['member'])
+    });
 
+    this.title.set(this.route.firstChild?.snapshot?.title);
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe({
@@ -34,12 +32,5 @@ export class MemberDetailed implements OnInit{
         this.title.set(this.route.firstChild?.snapshot?.title);
       }
     });
-  }
-
-  loadMember() {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (!id) return;
-
-    return this.memberService.getMember(id);
   }
 }
