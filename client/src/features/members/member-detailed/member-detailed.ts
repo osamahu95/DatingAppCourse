78e8710565fc64144a146796e669d45c97440711
osamahu95 <osamahu95@gmail.com>
2026-07-08 11:09:51 +0500

    udemy/#dotnet_with_angular/section9_fixed_memory_leak_when_subscribe_router_events_stream_using_takeUntilDestroyed

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-member-detailed',
@@ -20,6 +21,7 @@ export class MemberDetailed implements OnInit{
   private router = inject(Router);
   protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
+  private destroyRef = inject(DestroyRef);
   
   ngOnInit(): void {
     this.activatedRoute.data.subscribe({
@@ -28,7 +30,8 @@ export class MemberDetailed implements OnInit{
 
     this.title.set(this.activatedRoute.firstChild?.snapshot?.title);
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
+      filter(event => event instanceof NavigationEnd),
+      takeUntilDestroyed(this.destroyRef)
     ).subscribe({
       next: () => {
         this.title.set(this.activatedRoute.firstChild?.snapshot?.title);
