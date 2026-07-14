895b9635c10c491220af6edd6e425959f4e00c07
osamahu95 <osamahu95@gmail.com>
2026-07-14 13:32:04 +0500

    udemy/#dotnet_with_angular/section10_UpdateMember_controller_get_current_user_claims_user_id_which_is_of_course_similar_to_member_id_has_shared_primarykey_and_created_MemberUpdateDto

--- DatingApp/API/Controllers/MembersController.cs
+++ DatingApp/API/Controllers/MembersController.cs
@@ -1,7 +1,9 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -32,6 +34,26 @@ namespace API.Controllers
         {
             var memberPhotos = await membersRepository.GetPhotosForMemberAsync(id);
             return Ok(memberPhotos);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (memberId == null) return BadRequest("Oops - no id found in token");
+
+            var member = await membersRepository.GetMemberByIdAsync(memberId);
+            if (member == null) return BadRequest("Could not get Member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            membersRepository.Update(member); // optional 
+            if (await membersRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to Update Member");
         }
     }
 }
